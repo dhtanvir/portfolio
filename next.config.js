11fad1605c1a0965581c203189d385556993aefe
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2025-11-08 00:37:56 +0600

    Vercel config update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
   images: {
     unoptimized: true,
   },
-  basePath: '/portfolio', 
-  assetPrefix: '/portfolio/', 
 };
 
 export default nextConfig;
